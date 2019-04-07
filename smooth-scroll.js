8a41d0e35a0397d5f0240691ab5cd05ac165ef19
Joshua van Boxtel <joshua.vanboxtel@gmail.com>
2019-04-07 18:41:37 -0400

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -3,23 +3,30 @@ setTimeout( function() { scroll(0,0); }, 1);
 
 (function($) {
 
-    $('a').on('click', function(e) {
-        if (this.hash !== "") {            
-            e.preventDefault();
-            var hash    = this.hash;
-            smoothScroll(hash);
-        }
+    $('a[href^="#"]')
+        // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .on('click', function(e) {         
+        e.preventDefault();
+        var hash    = this.hash;
+        smoothScroll(hash);
     });
 
     if(window.location.hash) {
         smoothScroll(window.location.hash);
     }
 
-    function smoothScroll(hash) {        
-        var offset  = $('.header').outerHeight() + 10;
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top - offset
-        }, 800);
+    function smoothScroll(hash) {  
+        var target  = $(hash);
+        if (target.length) {   
+            var offset  = $('.header').outerHeight() + 10;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - offset
+            }, 800);
+        } else {
+            console.log('cannot find target: '+hash);
+        }
     }
 
 })( jQuery ); 
