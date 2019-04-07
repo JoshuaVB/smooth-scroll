4abd2f35bd8a6d9f3ffcf1f3b28947aaad73b16a
Joshua van Boxtel <joshua.vanboxtel@gmail.com>
2019-04-07 18:33:46 -0400

    Update smooth-scroll.js

--- smooth-scroll.js
+++ smooth-scroll.js
@@ -1,21 +1,21 @@
 if ( window.location.hash ) scroll(0,0);
 setTimeout( function() { scroll(0,0); }, 1);
 
-$(function() {
+(function($) {
 
     $('a').on('click', function(e) {
         if (this.hash !== "") {            
             e.preventDefault();
-            smoothScroll();
+            var hash    = this.hash;
+            smoothScroll(hash);
         }
     });
 
     if(window.location.hash) {
-        smoothScroll();
+        smoothScroll(window.location.hash);
     }
 
-    function smoothScroll() {
-        var hash    = this.hash;
+    function smoothScroll(hash) {        
         var offset  = $('.header').outerHeight() + 10;
         $('html, body').animate({
             scrollTop: $(hash).offset().top - offset
